0a542f00bece82097ff9ba81a237292482992a13
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2025-09-19 16:28:03 +0200

    feat: :sparkles: integrate Tailwind CSS and PostCSS, add autoprefixer, and update main application styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css'
+import '@/assets/index.css'
 
 import App from './App.vue'
 import router from './router'
